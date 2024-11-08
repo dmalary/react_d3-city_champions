d3497a4e6a54b45ce67fb499503e47f7c512ff56
madhat5 <davidmalary@gmail.com>
2024-11-07 19:54:40 -0500

    feat: annotation

--- components/LegendDonut.jsx
+++ components/LegendDonut.jsx
@@ -18,9 +18,10 @@ const colorScale = d3.scaleOrdinal()
   .domain([0, 1, 2, 3])
   .range(['#ffd147', '#ff58ba', '#39aaff', '#0a0527'])
 
-const LegendDonut = ({ key, width, height, data }) => {
+const LegendDonut = ({ keyProp, width, height, data }) => {
+  console.log('data', data)
 
-  const radius = Math.min(width, height) / 2 - specs.margin.global;
+  const radius = Math.min(width, height) / 2.25 - specs.margin.global;
   // console.log('radius', radius)
 
   const pie = useMemo(() => {
@@ -34,16 +35,16 @@ const LegendDonut = ({ key, width, height, data }) => {
 
   const shapes = pie.map((p, i) => {
     const sliceInfo = {
-      innerRadius: 20,
+      innerRadius: 15,
       outerRadius: radius,
       startAngle: p.startAngle,
       endAngle: p.endAngle
     };
 
     const centroid = arcPathGenerator.centroid(sliceInfo);
-    console.log('centroid', centroid)
+    // console.log('centroid', centroid)
     const slicePath = arcPathGenerator(sliceInfo);
-    console.log('slicePath', slicePath)
+    // console.log('slicePath', slicePath)
 
     const legendInfo = {
       innerRadius: radius + specs.padding,
@@ -53,12 +54,12 @@ const LegendDonut = ({ key, width, height, data }) => {
     };
 
     const legendPoint = arcPathGenerator.centroid(legendInfo);
-    console.log('legendPoint', legendPoint)
+    // console.log('legendPoint', legendPoint)
 
     const isRightLabel = legendPoint[0] > 0;
-    const labelX = legendPoint[0] + 50 * (isRightLabel ? 1 : -1);
+    const labelX = legendPoint[0] + 3 * (isRightLabel ? 1 : -1);
     const textAnchor = isRightLabel ? "start" : "end";
-    const label = `${p.data.name} - ${(Math.round(p.data.value * 100)).toFixed(2)}%`;
+    const label = `${p.data.name} - ${(Math.round(p.data.value * 100)).toFixed(0)}%`;
 
     return (
       <g key={i}>
@@ -68,7 +69,7 @@ const LegendDonut = ({ key, width, height, data }) => {
           x1={centroid[0]} 
           y1={centroid[1]} 
           x2={legendPoint[0]} 
-          y2={legendInfo[1]} 
+          y2={legendPoint[1]} 
           stroke={'#000'} 
           fill={'#000'}
         />
@@ -85,7 +86,7 @@ const LegendDonut = ({ key, width, height, data }) => {
           y={legendPoint[1]}
           textAnchor={textAnchor}
           dominantBaseline={'middle'}
-          fontSize={12}
+          fontSize={11}
         >
           {label}
         </text>
@@ -96,10 +97,17 @@ const LegendDonut = ({ key, width, height, data }) => {
   // console.log('arcs', arcs)
 
   return (
-    <svg key={key} width={width} height={height} style={{display: "inline-block"}}>
+    <svg key={keyProp} width={width} height={height} style={{display: "inline-block"}}>
       <g transform={`translate(${width / 2}, ${height / 2})`}>
         {shapes}
       </g>
+      <text
+        x={width / 3 + ((data.city).length)}
+        y={height - 3}
+        fontSize={13}
+      >
+        {data.city}
+      </text>
     </svg>
   )
 }
--- components/SmallMultiples.jsx
+++ components/SmallMultiples.jsx
@@ -1,16 +1,18 @@
 /* eslint-disable react/prop-types */
 
-import DonutChart from "../components/DonutChart";
+// import DonutChart from "../components/DonutChart";
 import LegendDonut from "../components/LegendDonut";
 
+const chartSpecs = {
+  width: 200,
+  height: 140,
+}
 
 const SmallMultiples = ({ data }) => {
-  
-  console.log('data', data)
+  // console.log('data', data)
 
-  const groups = data.map(el => el.city);
-  console.log('groups', groups);
-
+  // const groups = data.map(el => el.city);
+  // // console.log('groups', groups);
 
   const legendData = {
     sports:[
@@ -18,23 +20,24 @@ const SmallMultiples = ({ data }) => {
       {name: 'nfl', value: 0.25},
       {name: 'nba', value: 0.25},
       {name: 'nhl', value: 0.25},
-    ]
+    ],
+    city: 'template'
   }
   return (
-    <div 
+    <div className="chart-grid"
       style={{
-        width: 1100,
-        height: 800,
+        width: 1200,
+        // height: 1000,
         display: 'grid',
         gridTemplateColumns: 'repeat(7, minmax(0, 1fr))' 
       }}
     >
       {/* <DonutChart key={0} width={120} height={120} data={legendData} /> */}
-      <LegendDonut key={0} width={120} height={120} data={legendData} />
+      <LegendDonut key={0} width={chartSpecs.width} height={chartSpecs.height} data={legendData} />
       
-      { data.map((datum, i) => ( <DonutChart key={i} width={120} height={120} data={datum} /> ))}
+      {/* { data.map((datum, i) => ( <DonutChart key={i} width={chartSpecs.width} height={chartSpecs.height} data={datum} /> ))} */}
       
-      {/* { data.map((datum, i) => ( <LegendDonut key={i} width={120} height={120} data={datum} /> ))} */}
+      { data.map((datum, i) => ( <LegendDonut key={i} width={chartSpecs.width} height={chartSpecs.height} data={datum} /> ))}
     </div>
   )
 }
